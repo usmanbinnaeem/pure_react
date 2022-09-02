5d16fdf3a68c17336efa02f6f5796c158f847728
usmanbinnaeem <usmaanbinnaeem@gmail.com>
2022-09-02 15:37:10 +0500

    completed github List exercise

--- github-file-list/src/index.js
+++ github-file-list/src/index.js
@@ -1,17 +1,117 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import PropTypes from "prop-types";
+import Time from "./Time";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const testFiles = [
+  {
+    id: 1,
+    name: "src",
+    type: "folder",
+    updated_at: "2016-07-11 21:24:00",
+    latestCommit: {
+      message: "Initial commit",
+    },
+  },
+  {
+    id: 2,
+    name: "tests",
+    type: "folder",
+    updated_at: "2016-07-11 21:24:00",
+    latestCommit: {
+      message: "Initial commit",
+    },
+  },
+  {
+    id: 3,
+    name: "README",
+    type: "file",
+    updated_at: "2016-07-18 21:24:00",
+    latestCommit: {
+      message: "Added a readme",
+    },
+  },
+];
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const App = () => {
+  return <FileList files={testFiles} />;
+};
+
+const FileList = ({ files }) => {
+  return (
+    <table className="file-list">
+      <tbody>
+        {files.map((file) => (
+          <FileListItem key={file.id} file={file} />
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+FileList.propTypes = {
+  files: PropTypes.array,
+};
+
+const FileListItem = ({ file }) => {
+  return (
+    <tr className="file-list-item">
+      <FileName file={file} />
+      <CommitMessage commit={file.latestCommit} />
+      <td className="age">
+        <Time time={file.updated_at} />
+      </td>
+    </tr>
+  );
+};
+
+FileListItem.propTypes = {
+  file: PropTypes.object.isRequired,
+};
+
+function FileIcon({ file }) {
+  let icon = "fa-file-text-o";
+  if (file.type === "folder") {
+    icon = "fa-folder";
+  }
+  return (
+    <td className="file-icon">
+      <i className={`fa ${icon}`} />
+    </td>
+  );
+}
+FileIcon.propTypes = {
+  file: PropTypes.object.isRequired,
+};
+
+function FileName({ file }) {
+  return (
+    <>
+      <FileIcon file={file} />
+      <td className="file-name">{file.name}</td>
+    </>
+  );
+}
+FileName.propTypes = {
+  file: PropTypes.object.isRequired,
+};
+
+const CommitMessage = ({ commit }) => {
+  return <td className="commit-message">{commit.message}</td>;
+};
+
+CommitMessage.propTypes = {
+  commit: PropTypes.object.isRequired,
+};
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
