cacd6e9b689916e577f7892698b7f69355ffb654
usmanbinnaeem <usmaanbinnaeem@gmail.com>
2022-09-05 06:50:35 -0700

    completed useState"

--- usestate/src/index.js
+++ usestate/src/index.js
@@ -1,27 +1,92 @@
-import {useState, StrictMode} from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import { useState, StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const OneTimeButton = ({onClick}) => {
-  const [clicked, setClicked] = useState(false)
-  const handleClick = () => {
-    onClick()
-    setClicked(true)
-  }
+const StepTracker = () => {
+  const [steps, setSteps] = useState(0);
+
+  const increment = () => {
+    setSteps((steps) => steps + 1);
+  };
 
   return (
-    <button onClick={handleClick} disabled={clicked}>
-      You can only click me once
-    </button>
-  )
- 
+    <div>
+      You have taken {steps} step
+      <br />
+      <button onClick={increment}>I took another step</button>
+    </div>
+  );
+};
+
+function RandomList() {
+  const [items, setItems] = useState([]);
+
+  const addItem = () => {
+    setItems([
+      ...items,
+      {
+        id: items.length,
+        value: Math.random() * 100,
+      },
+    ]);
+  };
+
+  return (
+    <>
+      <button onClick={addItem}>Add a number</button>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>{item.value}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const MultiCounter = () => {
+  const [counts, setCounts] = useState({
+    countA: 0,
+    countB: 0,
+  });
+
+  const incA = () => {
+    setCounts((counts) => ({
+      ...counts,
+      countA: counts.countA + 1,
+    }));
+  };
+
+  const incB = () => {
+    setCounts((counts) => ({
+      ...counts,
+      countB: counts.countB + 1,
+    }));
+  };
+
+  const badIncA = () =>
+    setCounts({
+      countA: counts.countA + 1,
+    });
+
+  return (
+    <>
+      <div>A: {counts.countA}</div>
+      <div>B: {counts.countB}</div>
+      <button onClick={incA}>Increment A</button>
+      <button onClick={incB}>Increment B</button>
+
+      <button onClick={badIncA}>Bad Increment B</button>
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <OneTimeButton onClick={() => alert('hi')} />
+    <StepTracker onClick={() => alert("hi")} />
+    <RandomList />
+    <MultiCounter />
   </StrictMode>
 );
 
