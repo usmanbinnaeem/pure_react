f1e9b513b32482d98ecac7cfa1129473cad56001
usmanbinnaeem <usmaanbinnaeem@gmail.com>
2022-09-05 22:56:34 -0700

    exe-1 done

---
+++ usestate-exe/src/Room.jsx
@@ -0,0 +1,19 @@
+import {useState} from 'react'
+
+const Room = () => {
+    const [toggleLight, setToggleLight] = useState(false)
+
+    return (
+        <>
+        <center>
+            <h3>The Room is {toggleLight ? "Lit" : "Dark"}</h3>
+            <button onClick = {() => {
+            setToggleLight(!toggleLight)
+        }} >Switch Light</button>
+        </center>
+        </>
+    )
+    
+}
+
+export default Room;
